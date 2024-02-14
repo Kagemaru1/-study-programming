b672b8ecad04f9db96e777fc5eecf7e28875509d
Viktor Stoev <76692453+Kagemaru1@users.noreply.github.com>
2024-02-14 21:44:58 +0200

    Tasks

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Study-Programming
 07.01.2024
 Today i decided to learn programming!!!
+
+Day 1
+1:15 hours Study time!
+Basics of HTML and JS.
+
+day 2
--- SoftUni Basics/ProgrammingBasicsOnlineRetakeExam-2And3May2019/01.FruitMarket.js
+++ SoftUni Basics/ProgrammingBasicsOnlineRetakeExam-2And3May2019/01.FruitMarket.js
@@ -5,13 +5,13 @@ function fruitMarket(strawberryPrice, bananasKg, orangesKg, redRaspberryKg, stra
     // let redRaspberryKg = Number(input[3]);
     // let strawberryKg = Number(input[4]);
 
-    let redRaspberryPrice = strawberryPrice / 2;
-    let orangesPrice = redRaspberryPrice * 0.6;
-    let bananasPrice = redRaspberryPrice * 0.2;
+    let redRaspberryPrice = Number(strawberryPrice) / 2;
+    let orangesPrice = Number(redRaspberryPrice) * 0.6;
+    let bananasPrice = Number(redRaspberryPrice) * 0.2;
 
     let finaleCost = strawberryKg * strawberryPrice + bananasKg * bananasPrice + orangesKg * orangesPrice + redRaspberryKg * redRaspberryPrice;
 
-    console.log(finaleCost);
+    console.log(finaleCost.toFixed(2));
 }
-fruitMarket(["48", "10", "3.3", "6.5", "1.7"]);
-fruitMarket(["63.5", "3.57", "6.35", "8.15", "2.5"]);
+fruitMarket("48", "10", "3.3", "6.5", "1.7");
+fruitMarket("63.5", "3.57", "6.35", "8.15", "2.5");
